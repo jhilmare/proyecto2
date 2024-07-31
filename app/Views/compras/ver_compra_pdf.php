244603a131867312dc96bbbba84806db6dc71bd2
jhilmare <jhilmar7405@gmail.com>
2024-07-31 03:25:43 -0500

    Add files via upload

---
+++ app/Views/compras/ver_compra_pdf.php
@@ -0,0 +1,21 @@
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+
+
+
+
+            <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                    <div class="panel">
+                    <iframe src="<?php echo base_url() . "/compras/generaCompraPdf/" . $id_compra; ?>" frameborder="0" class="embed-responsive embed-responsive-4by3" style="margin-top: 30px;"></iframe>
+                    </div>
+                </div>
+        </div>
+       </div>
+      </div>        
+   </div>
+   </div>  
+   </div>  
